f9c883a0fa28504a585c7617b1b96d0d66502149
Wandersondu <wdutra@outlook.pt>
2022-04-02 00:13:25 +0100

    adicionando estruturas iniciais

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => {
+  const message = "Inicando react"
+  return <div className="container">{message}</div>;
+  
+}
+
+export default App; 
